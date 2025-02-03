bcd731b2e6eccd9b6f2f33bc3c4bb49b68ea9159
Vasco Pereira da Silva <902401149@portic.ipp.pt>
2025-02-03 14:10:37 +0000

    SpringBoot initial project Creation

---
+++ BidManager/src/main/java/pt/upskill/bidmanager/BidManagerApplication.java
@@ -0,0 +1,13 @@
+package pt.upskill.bidmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BidManagerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BidManagerApplication.class, args);
+    }
+
+}
---
+++ BidManager/src/test/java/pt/upskill/bidmanager/BidManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package pt.upskill.bidmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BidManagerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
